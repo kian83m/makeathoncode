8f8a5440d01c08d5cd405b2b37eb7ebdcdc1e275
Kian <kian.mahmoodi83@gmail.com>
2025-02-23 02:33:43 -0500

    a

--- main.py
+++ main.py
@@ -1,44 +1,77 @@
+# import LCD_1in44
+# import time
+
+# from PIL import Image,ImageDraw,ImageFont,ImageColor
+
+# #try:
+# def main():
+#     LCD = LCD_1in44.LCD()
+
+#     print ("**********Init LCD**********")
+#     Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
+#     LCD.LCD_Init(Lcd_ScanDir)
+#     LCD.LCD_Clear()
+
+#     image = Image.new("RGB", (LCD.width, LCD.height), "WHITE")
+#     draw = ImageDraw.Draw(image)
+#     #font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 16)
+#     print ("***draw line")
+#     draw.line([(0,0),(127,0)], fill = "BLUE",width = 5)
+#     draw.line([(127,0),(127,127)], fill = "BLUE",width = 5)
+#     draw.line([(127,127),(0,127)], fill = "BLUE",width = 5)
+#     draw.line([(0,127),(0,0)], fill = "BLUE",width = 5)
+#     print ("***draw rectangle")
+#     draw.rectangle([(18,10),(110,20)],fill = "RED")
+
+#     print ("***draw text")
+#     draw.text((33, 22), 'WaveShare ', fill = "BLUE")
+#     draw.text((32, 36), 'Electronic ', fill = "BLUE")
+#     draw.text((28, 48), '1.44inch LCD ', fill = "BLUE")
+
+#     LCD.LCD_ShowImage(image,0,0)
+#     time.sleep(3)
+
+#     image = Image.open('time.bmp')
+#     LCD.LCD_ShowImage(image,0,0)
+#     time.sleep(3)
+# 	#while (True):
+	
+# if __name__ == '__main__':
+#     main()
+
+
 import LCD_1in44
 import time
+from PIL import Image, ImageDraw, ImageFont
 
-from PIL import Image,ImageDraw,ImageFont,ImageColor
-
-#try:
 def main():
+    # Initialize the LCD display
     LCD = LCD_1in44.LCD()
-
-    print ("**********Init LCD**********")
-    Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
+    print("********** Init LCD **********")
+    Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT  # Default scan direction
     LCD.LCD_Init(Lcd_ScanDir)
     LCD.LCD_Clear()
 
-    image = Image.new("RGB", (LCD.width, LCD.height), "WHITE")
+    # Create a new image with a red background
+    image = Image.new("RGB", (LCD.width, LCD.height), "RED")
     draw = ImageDraw.Draw(image)
-    #font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 16)
-    print ("***draw line")
-    draw.line([(0,0),(127,0)], fill = "BLUE",width = 5)
-    draw.line([(127,0),(127,127)], fill = "BLUE",width = 5)
-    draw.line([(127,127),(0,127)], fill = "BLUE",width = 5)
-    draw.line([(0,127),(0,0)], fill = "BLUE",width = 5)
-    print ("***draw rectangle")
-    draw.rectangle([(18,10),(110,20)],fill = "RED")
 
-    print ("***draw text")
-    draw.text((33, 22), 'WaveShare ', fill = "BLUE")
-    draw.text((32, 36), 'Electronic ', fill = "BLUE")
-    draw.text((28, 48), '1.44inch LCD ', fill = "BLUE")
+    # Draw two eyes for the sad face
+    # Left eye
+    draw.ellipse((40, 40, 50, 50), fill="WHITE", outline="BLACK")
+    # Right eye
+    draw.ellipse((78, 40, 88, 50), fill="WHITE", outline="BLACK")
 
-    LCD.LCD_ShowImage(image,0,0)
+    # Draw a frowning mouth using an arc
+    # The arc is drawn from 200 to 340 degrees to form a downward curve (frown)
+    draw.arc((40, 70, 90, 110), start=200, end=340, fill="WHITE", width=2)
+
+    # Draw text "sad" below the face
+    draw.text((50, 115), "sad", fill="WHITE")
+    
+    # Display the image on the LCD
+    LCD.LCD_ShowImage(image, 0, 0)
     time.sleep(3)
 
-    image = Image.open('time.bmp')
-    LCD.LCD_ShowImage(image,0,0)
-    time.sleep(3)
-	#while (True):
-	
 if __name__ == '__main__':
     main()
-
-#except:
-#	print("except")
-#	GPIO.cleanup()
